d6cf0b028087bafdd5d9ac6124396b82b7602858
niccololampa <niccololampa@gmail.com>
2022-10-20 01:14:02 +0800

    Add initial search plugin interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Amazon Price Averager</h1>
+        <label>
+          Item Name:
+          <input type="text" name="item" />
+        </label>
+        <button type="button">Get Average</button>
       </header>
     </div>
   )

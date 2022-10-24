4ccaadb2af1d48efcc1cc0b4387872091ca5cb78
niccololampa <niccololampa@gmail.com>
2022-10-24 14:12:37 +0800

    Update @types/cheerio to use cheerio.TextElement type for el in helper

--- server/helper.ts
+++ server/helper.ts
@@ -13,12 +13,15 @@ const fetchItemPrices = async (item: string, page: number) => {
 
         $(
             'div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20'
-        ).each((_idx: number, el: any) => {
+        ).each((_idx: number, el: cheerio.TextElement) => {
             const element = $(el)
             const price = element.find('span.a-price > span.a-offscreen').text()
             // regex to get the price.
             if (price) {
-                prices.push(parseFloat(price.match(/(?<=\$)[\d.]+/)[0]))
+                const regexPrice = price.match(/(?<=\$)[\d.]+/)
+                if (regexPrice) {
+                    prices.push(parseFloat(regexPrice[0]))
+                }
             }
         })
 
